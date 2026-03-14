6117a11c2db9681f497a7a2156aac0f56a01c723
denis-k2 <dxxx999@gmail.com>
2026-03-14 17:34:32 +0700

    Добавлен уникальный индекс на (category_id, param) в numbeo_param

--- sql/create_numbeo_param.sql
+++ sql/create_numbeo_param.sql
@@ -3,5 +3,6 @@ CREATE TABLE numbeo_param (
     category_id int NOT NULL,
     param varchar(100) NOT NULL,
     CONSTRAINT PK_param_id PRIMARY KEY ( param_id ),
-    CONSTRAINT fk_category_id FOREIGN KEY ( category_id ) REFERENCES numbeo_category ( category_id )
+    CONSTRAINT fk_category_id FOREIGN KEY ( category_id ) REFERENCES numbeo_category ( category_id ),
+    CONSTRAINT uq_numbeo_param_category_param UNIQUE (category_id, param)
 );
